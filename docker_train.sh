f912a40452e823a8d49459be4f6178114df06f73
pirunita <ksparchive39@gmail.com>
2020-04-20 13:31:00 +0900

    Docker build container file

---
+++ docker_train.sh
@@ -0,0 +1,2 @@
+docker run --runtime=nvidia -it --rm -v ~/hdd_ext/DeepLearning/wsddn/pytorch-wsddn2:/wsddn --ip host ufoym/deepo:all-py36-cu90
+
---
+++ docker_wsddn_train.sh
@@ -0,0 +1,1 @@
+docker run --runtime=nvidia -it --name wsddn -v ~/hdd_ext/DeepLearning/wsddn/pytorch-wsddn2:/wsddn --ip host wsddn
