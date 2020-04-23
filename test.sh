047cd1b656a2988fa4642198baf553f6c766ddcf
pirunita <ksparchive39@gmail.com>
2020-04-23 13:45:08 +0900

    Run shell file

---
+++ test.sh
@@ -0,0 +1,5 @@
+python run.py \
+--mode test \
+--session 2 \
+--ssw_path voc_2007_test.mat \
+--image_label_path voc_2007_test_image_label.json \
---
+++ train.sh
@@ -0,0 +1,5 @@
+python run.py \
+--mode train \
+--session 3 \
+--ssw_path voc_2007_trainval.mat \
+--image_label_path voc_2007_trainval_image_label.json \
